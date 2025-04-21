eaf9030c677be9065835e1146d782b0b791c06d6
reewaj <r.waj555@gmail.com>
2025-04-21 13:36:14 +0545

    ui optimises

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -1,3 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
+
 export default function EventsPage() {
+    const { userId, redirectToSignIn } = auth()
+
+    if (userId == null) return redirectToSignIn()
+
+
     return <h1>Events Page</h1>;
 }
